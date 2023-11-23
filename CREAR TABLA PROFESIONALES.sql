a7cf7ba3cd2cc27c578a58fc1ed6f8e8b198f439
cunita-91 <combet606@gmail.com>
2023-11-23 19:35:33 -0300

    Declaracion FK

--- CREAR TABLA PROFESIONALES.sql
+++ CREAR TABLA PROFESIONALES.sql
@@ -1,4 +1,4 @@
-CREATE TABLE TurnosCardio.Profesionales (
+CREATE TABLE turnoscardio.Profesionales (
     ID_profesional INT AUTO INCREMENT NOT NULL PRIMARY KEY,
     Apellido_prof VARCHAR (30) NOT NULL,
     Nombre_prof VARCHAR (30) NOT NULL,
