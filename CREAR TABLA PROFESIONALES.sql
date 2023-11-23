90267ecb4d790ee0335fb8431c8d1f828ae30ea4
cunita-91 <combet606@gmail.com>
2023-11-23 18:59:30 -0300

    Crear tabla prfoseionales

---
+++ CREAR TABLA PROFESIONALES.sql
@@ -0,0 +1,6 @@
+CREATE TABLE TurnosCardio.Profesionales (
+    ID_profesional INT AUTO INCREMENT NOT NULL PRIMARY KEY,
+    Apellido_prof VARCHAR (30) NOT NULL,
+    Nombre_prof VARCHAR (30) NOT NULL,
+    Especialidad VARCHAR (30) NOT NULL,
+    Dia_atencion DATE NOT NULL);
